7ad2dc1d90e7d4ac030b5d43b0e44f9eeebef94d
xoannguyen20030709-a11y <xoannguyen20030709@gmail.com>
2026-05-28 15:39:58 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Kill old VNC if exists
 vncserver -kill :1 >/dev/null 2>&1 || true
 
+# Start audio
 pulseaudio --start || true
 
+# Start VNC
 vncserver :1 \
   -geometry ${VNC_RESOLUTION} \
   -depth ${VNC_COL_DEPTH}
 
+# Start noVNC
 websockify \
   --web=/usr/share/novnc/ \
   --heartbeat=30 \
